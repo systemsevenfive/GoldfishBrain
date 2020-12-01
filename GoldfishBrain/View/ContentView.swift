0e3972ddeee1a092d74254f9cac3be2036c2df23
Ryan Saunders <r.saunders@icloud.com>
2020-11-30 22:38:46 -0500

    Core Data retrieval and deletion

--- GoldfishBrain/View/ContentView.swift
+++ GoldfishBrain/View/ContentView.swift
@@ -11,32 +11,59 @@ struct ContentView: View {
     //MARK: Properties
     
     @Environment (\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(entity: ToDoItem.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ToDoItem.name, ascending: true)]) var ToDoItems: FetchedResults<ToDoItem>
     @State private var showingAddItemView: Bool = false
     
     
     //MARK: Body
     var body: some View {
         NavigationView {
-            List(0..<5) { item in
-                Text("Hello, world!")
+            List {
+                ForEach(self.ToDoItems, id: \.self) { toDoItem in
+                    HStack {
+                        Text(toDoItem.name ?? "Unknown")
+                        Spacer()
+                        Text(toDoItem.priority ?? "Unknown")
+                    }
+                }// ForEach
+                .onDelete(perform: deleteToDoItem)
             }// List
             .navigationBarTitle("Stuff to do", displayMode: .inline)
-            .navigationBarItems(trailing:
-                Button(action: {
-                    self.showingAddItemView.toggle()
-                }) {
-                    Image(systemName: "plus")
-                }//: Add button
-                .sheet(isPresented: $showingAddItemView) {
-                    AddItemView().environment(\.managedObjectContext, self.managedObjectContext)
-                }
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing:
+                    Button(action: {
+                        self.showingAddItemView.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                    }//: Add button
+                    .sheet(isPresented: $showingAddItemView) {
+                        AddItemView().environment(\.managedObjectContext, self.managedObjectContext)
+                    }
             )
         }//: Navigation
+    }
+    private func deleteToDoItem(at offsets: IndexSet) {
+        for index in offsets {
+            let toDoItem = ToDoItems[index]
+            managedObjectContext.delete(toDoItem)
+            
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let context = (UIApplication.shared.delegate as!
+                        AppDelegate).persistentContainer.viewContext
+        return ContentView()
+            .environment(\.managedObjectContext, context)
+            .previewDevice("iPhone 11 Pro Max")
     }
 }
